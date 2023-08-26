fcd8f60db2961e350f213691b7f893eb2c5db77e
Matheus Augusto <matheuspaulasoares10@gmail.com>
2023-08-25 21:47:16 -0300

    adiciona tema, estilos globais e corrige importações inexistentes

---
+++ src/styles/theme/colors/index.ts
@@ -0,0 +1,22 @@
+export const colors = {
+  brand: {
+    primary: " #3E2723",
+    secondary: "#8D6E63",
+  },
+  highlight: {
+    primary: "#795548",
+    secondary: "#D84315",
+  },
+  textColor: {
+    primary: "#FFF3E0",
+  },
+  accentColor: {
+    primary: "#BDBDBD",
+    secondary: "#FF5722",
+    tertiary: "#C62828",
+  },
+  neutralColor: {
+    primary: "#616161",
+    secondary: "#E0E0E0",
+  },
+};
